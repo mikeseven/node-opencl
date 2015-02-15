a958214e69d8b3baad3a11cb56529db59dcc8f47
fredericlb <fred@ioweb.fr>
2015-02-15 15:07:58 +0100

    [Implementation] Strong types check and Node 0.12 support for Sampler

--- src/sampler.cpp
+++ src/sampler.cpp
@@ -1,4 +1,5 @@
 #include "sampler.h"
+#include "types.h"
 
 namespace opencl {
 
@@ -14,24 +15,28 @@ NAN_METHOD(CreateSampler) {
   NanScope();
   REQ_ARGS(4);
 
-  if(!isOpenCLObj(args[0])) {
-    return NanThrowError(JS_INT(CL_INVALID_CONTEXT));
-  }
-  cl_context context=Unwrap<cl_context>(args[0]);
+  // Arg 0
+  NOCL_UNWRAP(context, NoCLContext, args[0]);
+
+  // Arg 1
   cl_bool normalized_coords = args[1]->BooleanValue() ? CL_TRUE : CL_FALSE;
+
+  // Arg 2
   cl_addressing_mode addressing_mode = args[2]->Uint32Value();
+
+  // Arg 3
   cl_filter_mode filter_mode = args[3]->Uint32Value();
 
   cl_int ret=CL_SUCCESS;
   cl_sampler sw = ::clCreateSampler(
-              context,
+              context->getRaw(),
               normalized_coords,
               addressing_mode,
               filter_mode,
               &ret);
   CHECK_ERR(ret);
 
-  NanReturnValue(Wrap(sw));
+  NanReturnValue(NOCL_WRAP(NoCLSampler, sw));
 }
 
 #else
@@ -39,6 +44,11 @@ NAN_METHOD(CreateSampler) {
 // clCreateSamplerWithProperties(cl_context                     /* context */,
 //                               const cl_sampler_properties *  /* normalized_coords */,
 //                               cl_int *                       /* errcode_ret */) CL_API_SUFFIX__VERSION_2_0;
+NAN_METHOD(CreateSamplerWithProperties) {
+  NanScope();
+  REQ_ARGS(4);
+}
+
 #endif
 
 // extern CL_API_ENTRY cl_int CL_API_CALL
@@ -47,12 +57,8 @@ NAN_METHOD(RetainSampler) {
   NanScope();
   REQ_ARGS(1);
 
-  if(!isOpenCLObj(args[0])) {
-    return NanThrowError(JS_INT(CL_INVALID_SAMPLER));
-  }
-
-  cl_sampler sampler = Unwrap<cl_sampler>(args[0]);
-  cl_int count=clRetainSampler(sampler);
+  NOCL_UNWRAP(sampler, NoCLSampler, args[0]);
+  cl_int count=clRetainSampler(sampler->getRaw());
 
   NanReturnValue(JS_INT(count));
 }
@@ -63,12 +69,8 @@ NAN_METHOD(ReleaseSampler) {
   NanScope();
   REQ_ARGS(1);
 
-  if(!isOpenCLObj(args[0])) {
-    return NanThrowError(JS_INT(CL_INVALID_SAMPLER));
-  }
-
-  cl_sampler sampler = Unwrap<cl_sampler>(args[0]);
-  cl_int count=clReleaseSampler(sampler);
+  NOCL_UNWRAP(sampler, NoCLSampler, args[0]);
+  cl_int count=clReleaseSampler(sampler->getRaw());
 
   NanReturnValue(JS_INT(count));
 }
@@ -83,42 +85,39 @@ NAN_METHOD(GetSamplerInfo) {
   NanScope();
   REQ_ARGS(2);
 
-  if(!isOpenCLObj(args[0])) {
-    return NanThrowError(JS_INT(CL_INVALID_SAMPLER));
-  }
-  cl_sampler sampler=Unwrap<cl_sampler>(args[0]);
+  NOCL_UNWRAP(sampler, NoCLSampler, args[0]);
   cl_sampler_info param_name = args[1]->Uint32Value();
 
   switch(param_name) {
     case CL_SAMPLER_REFERENCE_COUNT:
     {
       cl_uint val;
-      CHECK_ERR(::clGetSamplerInfo(sampler,param_name,sizeof(cl_uint), &val, NULL))
+      CHECK_ERR(::clGetSamplerInfo(sampler->getRaw(),param_name,sizeof(cl_uint), &val, NULL))
       NanReturnValue(JS_INT(val));
     }
     case CL_SAMPLER_CONTEXT:
     {
       cl_context val;
-      CHECK_ERR(::clGetSamplerInfo(sampler,param_name,sizeof(cl_context), &val, NULL))
+      CHECK_ERR(::clGetSamplerInfo(sampler->getRaw(),param_name,sizeof(cl_context), &val, NULL))
       // TODO NanReturnValue(JS_INT(val));
       break;
     }
     case CL_SAMPLER_NORMALIZED_COORDS:
     {
       cl_bool val;
-      CHECK_ERR(::clGetSamplerInfo(sampler,param_name,sizeof(cl_bool), &val, NULL))
+      CHECK_ERR(::clGetSamplerInfo(sampler->getRaw(),param_name,sizeof(cl_bool), &val, NULL))
       NanReturnValue(JS_BOOL(val));
     }
     case CL_SAMPLER_ADDRESSING_MODE:
     {
       cl_addressing_mode val;
-      CHECK_ERR(::clGetSamplerInfo(sampler,param_name,sizeof(cl_addressing_mode), &val, NULL))
+      CHECK_ERR(::clGetSamplerInfo(sampler->getRaw(),param_name,sizeof(cl_addressing_mode), &val, NULL))
       NanReturnValue(JS_INT(val));
     }
     case CL_SAMPLER_FILTER_MODE:
     {
       cl_filter_mode val;
-      CHECK_ERR(::clGetSamplerInfo(sampler,param_name,sizeof(cl_filter_mode), &val, NULL))
+      CHECK_ERR(::clGetSamplerInfo(sampler->getRaw(),param_name,sizeof(cl_filter_mode), &val, NULL))
       NanReturnValue(JS_INT(val));
     }
   }
