6adb7ec0b795f5cf24a2d007566bafc7748e39e3
ptaylor <paul.e.taylor@me.com>
2019-01-11 01:23:53 -0800

    use updated node-memwatch, cleanup test_leak1 test

--- test/test_leak1.js
+++ test/test_leak1.js
@@ -1,18 +1,12 @@
 'use strict';
 
 var cl = require("../lib/opencl");
-var memwatch = require('memwatch-next');
+var memwatch = require('node-memwatch');
 
 process.on('exit', function dump() {
   memwatch.gc();
   console.info("Exit.");
 });
-
-memwatch.on('stats', function(stats) {
-  console.info("mem: "+stats.estimated_base+" trend: "+(stats.usage_trend>0 ? '+' : (stats.usage_trend>0 ? '-' : '=')));
-});
-
-var i=0;
 
 var platform=cl.getPlatformIDs()[0];
 var properties=[cl.CONTEXT_PLATFORM, platform];
@@ -26,26 +20,31 @@ else {
     cl.getDeviceIDs(platform, cl.DEVICE_TYPE_GPU));
 }
 
-memwatch.gc();
-while (i++ < 1000) {
-  var ev = cl.createUserEvent(ctx);
-  cl.releaseEvent(ev);
-  memwatch.gc();
-}
-memwatch.gc();
+test();
 
-// === another test ===
-//
-// function tick() {
-//   global.gc();
-//   var ev = cl.createUserEvent(ctx);
-//   cl.releaseEvent(ev);
-//   global.gc();
-//   if(i++ < 1000) setTimeout(tick,10);
-// };
+function test() {
 
-// var ctx = cl.createContextFromType(
-//  [cl.CONTEXT_PLATFORM, cl.getPlatformIDs()[0]], cl.DEVICE_TYPE_CPU, null, null);
+  var i = 0;
 
-// global.gc();
-// tick();
+  memwatch.on('stats', logStats);
+
+  loop();
+
+  function loop() {
+    memwatch.gc();
+    cl.releaseEvent(cl.createUserEvent(ctx));
+    memwatch.gc();
+    if(++i < 1000) {
+      setTimeout(loop,10);
+    } else {
+      memwatch.removeListener('stats', logStats);
+      process.exit(0);
+    }
+  }
+
+  function logStats(stats) {
+    console.info("mem: "+
+      (stats.estimated_base / (1024 ** 2)).toFixed(2) + "mb" +
+      " trend: "+(stats.usage_trend>0 ? '+' : (stats.usage_trend<0 ? '-' : '=')));
+  }
+};
